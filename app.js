d9ecaaf163aa1f132afe61af788e4628fa66bc8c
Andrea <andreabonola@gmail.com>
2019-10-24 21:56:21 -0400

    Created schema and connected to mongoDB.

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose")
+    mongoose = require("mongoose"),
+    Sofipics = require("./models/sofipics")
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/views"));
 
-var sofipics = [
-  {name: "Sofi Sleeps", image: "https://i.postimg.cc/kXBpVjfF/20-FE1-FC9-2-B33-45-B5-8-CA9-3-E7-A9-B3-DD484.jpg"},
-  {name: "Sofi Peeps", image: "https://i.postimg.cc/nhmS2X8X/AD57-E8-D4-CB38-4078-9-FD4-FCF7-ECE0-AC4-B.jpg"},
-  {name: "sofi don't Care", image: "https://i.postimg.cc/5NTk8wrc/879-CE9-C2-2-FF6-4051-AE69-B85702-A42-E35.jpg"},
-  {name: "Sofi Sleeps", image: "https://i.postimg.cc/kXBpVjfF/20-FE1-FC9-2-B33-45-B5-8-CA9-3-E7-A9-B3-DD484.jpg"},
-  {name: "Sofi Peeps", image: "https://i.postimg.cc/nhmS2X8X/AD57-E8-D4-CB38-4078-9-FD4-FCF7-ECE0-AC4-B.jpg"},
-  {name: "sofi don't Care", image: "https://i.postimg.cc/5NTk8wrc/879-CE9-C2-2-FF6-4051-AE69-B85702-A42-E35.jpg"},
-  {name: "Sofi Sleeps", image: "https://i.postimg.cc/kXBpVjfF/20-FE1-FC9-2-B33-45-B5-8-CA9-3-E7-A9-B3-DD484.jpg"},
-  {name: "Sofi Peeps", image: "https://i.postimg.cc/nhmS2X8X/AD57-E8-D4-CB38-4078-9-FD4-FCF7-ECE0-AC4-B.jpg"},
-  {name: "sofi don't Care", image: "https://i.postimg.cc/5NTk8wrc/879-CE9-C2-2-FF6-4051-AE69-B85702-A42-E35.jpg"}
-];
 
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/sofipics", function(req, res){
-  res.render("sofipics", {sofipics: sofipics});
+    Sofipics.find({}, function(err, allSofiPics){
+      if(err){
+        console.log(err);
+      } else{
+        res.render("sofipics", {sofipics: allSofiPics});
+      }
+    });
 });
 
 app.post("/sofipics", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
   var newSofiPic = {name: name, image: image}
-  sofipics.push(newSofiPic);
+  Sofipics.create(newSofiPic, function(err, newlyCreated){
+    if(err){
+      console.log(err)
+    } else{
+      res.redirect("./sofipics");
+    }
+  });
   res.redirect("/sofipics");
 });
 
